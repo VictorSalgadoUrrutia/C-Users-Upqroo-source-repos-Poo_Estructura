9e03a76432ea7e831e84254beb4d0a226a03cd63
ArthurFkOne <arturohrh@gmail.com>
2023-05-18 15:57:52 -0500

    Se agrega mi tarea

---
+++ Services/AlumnoServices.cs
@@ -0,0 +1,31 @@
+﻿using Poo_Estructura.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_Estructura.Services
+{
+    public class AlumnoServices
+    {
+        public Alumno Agregar()
+        {
+            try {
+                Alumno res = new Alumno()
+                {
+                    Id = 1,
+                    Nombre = "Victor",
+                    Apellido = "Salgado",
+                    FechaIngreso = DateTime.Now,
+                    Grado = 3,
+                    Promedio = 9.5
+                };
+                return res;
+            } 
+            catch (Exception ex) {
+                throw new Exception("Sucedio un error"+ex.Message)
+            }
+        }
+    }
+}
